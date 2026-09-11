4e52dfb5d2baf190ed5d2b2932eeb3e34fd3b768
PLANNING AND DESIGN SECTION <christine.dpwh2024@gmail.com>
2026-09-11 15:05:08 +0800

    Update monitoring-data.js

--- monitoring-data.js
+++ monitoring-data.js
@@ -1,6 +1,297 @@
 /* =========================================================
    PDS PROJECT MONITORING DATA
-   Source: Monitoring Excel
+   GITHUB-BASED MONITORING
 ========================================================= */
 
-const monitoringData = [];
+const monitoringData = [
+    {
+        contract_id: "26DD0001",
+        program2: "Alfred",
+        plan: "Karisa"
+    },
+    {
+        contract_id: "26DD0002",
+        program2: "Christian",
+        plan: "Francis"
+    },
+    {
+        contract_id: "26DD0003",
+        program2: "Bevelyn",
+        plan: "Jurell"
+    },
+    {
+        contract_id: "26DD0004",
+        program2: "JM",
+        plan: "Edwin"
+    },
+    {
+        contract_id: "26DD0005",
+        program2: "Christine",
+        plan: "Jessica"
+    },
+    {
+        contract_id: "26DD0006",
+        program2: "Johannes",
+        plan: "Joanne"
+    },
+    {
+        contract_id: "26DD0007",
+        program2: "Venson",
+        plan: "Carlo"
+    },
+    {
+        contract_id: "26DD0008",
+        program2: "Johanna",
+        plan: "Joanne"
+    },
+    {
+        contract_id: "26DD0009",
+        program2: "Johannes",
+        plan: "Mark"
+    },
+    {
+        contract_id: "26DD0010",
+        program2: "Mika",
+        plan: "Magnolia"
+    },
+    {
+        contract_id: "26DD0011",
+        program2: "Benedict",
+        plan: "JR"
+    },
+    {
+        contract_id: "26DD0012",
+        program2: "Lyra",
+        plan: "Anjeline"
+    },
+    {
+        contract_id: "26DD0013",
+        program2: "Christian",
+        plan: "Renzon"
+    },
+    {
+        contract_id: "26DD0014",
+        program2: "Christine",
+        plan: "Carlo"
+    },
+    {
+        contract_id: "26DD0015",
+        program2: "Benedict",
+        plan: "Magnolia"
+    },
+    {
+        contract_id: "26DD0016",
+        program2: "Mika",
+        plan: "Aero"
+    },
+    {
+        contract_id: "26DD0017",
+        program2: "Johanna",
+        plan: "Jethro"
+    },
+    {
+        contract_id: "26DD0018",
+        program2: "Johannes",
+        plan: "Jessica"
+    },
+    {
+        contract_id: "26DD0019",
+        program2: "Christian",
+        plan: "Francis"
+    },
+    {
+        contract_id: "26DD0020",
+        program2: "Neo",
+        plan: "Magnolia"
+    },
+    {
+        contract_id: "26DD0021",
+        program2: "Rizza",
+        plan: "Kei"
+    },
+    {
+        contract_id: "26DD0022",
+        program2: "Bevelyn",
+        plan: "Karisa"
+    },
+    {
+        contract_id: "26DD0023",
+        program2: "Lemuel",
+        plan: "Joanne"
+    },
+    {
+        contract_id: "26DD0024",
+        program2: "Bevelyn",
+        plan: "Mark"
+    },
+    {
+        contract_id: "26DD0025",
+        program2: "Lyra",
+        plan: "Kei"
+    },
+    {
+        contract_id: "26DD0026",
+        program2: "Christine",
+        plan: "Carlo"
+    },
+    {
+        contract_id: "26DD0027",
+        program2: "Lyra",
+        plan: "Karisa"
+    },
+    {
+        contract_id: "26DD0028",
+        program2: "Benedict",
+        plan: "Anjeline"
+    },
+    {
+        contract_id: "26DD0029",
+        program2: "Mika",
+        plan: "Magnolia"
+    },
+    {
+        contract_id: "26DD0030",
+        program2: "Cath",
+        plan: "Jurell"
+    },
+    {
+        contract_id: "26DD0031",
+        program2: "Benedict",
+        plan: "Magnolia"
+    },
+    {
+        contract_id: "26DD0032",
+        program2: "Christine",
+        plan: "Carlo"
+    },
+    {
+        contract_id: "26DD0033",
+        program2: "Bevelyn",
+        plan: "Rose Anne"
+    },
+    {
+        contract_id: "26DD0035",
+        program2: "Christian",
+        plan: "Edwin"
+    },
+    {
+        contract_id: "26DD0038",
+        program2: "Mika",
+        plan: "Jessica"
+    },
+    {
+        contract_id: "26DD0039",
+        program2: "Mika",
+        plan: "JR"
+    },
+    {
+        contract_id: "26DD0045",
+        program2: "Maintenance",
+        plan: "Renzon"
+    },
+    {
+        contract_id: "26DD0046",
+        program2: "Bevelyn",
+        plan: "Jessica"
+    },
+    {
+        contract_id: "26DD0050",
+        program2: "Benedict",
+        plan: "Jethro"
+    },
+    {
+        contract_id: "26DD0056",
+        program2: "Christian",
+        plan: "Kressel"
+    },
+    {
+        contract_id: "26DD0062",
+        program2: "Benedict",
+        plan: "Edwin"
+    },
+    {
+        contract_id: "26DD0066",
+        program2: "Johanna",
+        plan: "Rose Anne"
+    },
+    {
+        contract_id: "26DD0071",
+        program2: "Christine",
+        plan: "Anjeline"
+    },
+    {
+        contract_id: "26DD0073",
+        program2: "Benedict",
+        plan: "Renzon"
+    },
+    {
+        contract_id: "26DD0078",
+        program2: "Johannes",
+        plan: "Aizamay"
+    },
+    {
+        contract_id: "26DD0079",
+        program2: "Christian",
+        plan: "Kemmuel"
+    },
+    {
+        contract_id: "26DD0084",
+        program2: "Lyra",
+        plan: "Reynalyn"
+    },
+    {
+        contract_id: "26DD0085",
+        program2: "JM",
+        plan: "Kemmuel"
+    },
+    {
+        contract_id: "26DD0100",
+        program2: "Von",
+        plan: "Reynalyn"
+    },
+    {
+        contract_id: "26DD0117",
+        program2: "Angelou",
+        plan: "JR"
+    },
+    {
+        contract_id: "26DD0127",
+        program2: "Jonel",
+        plan: "Jurell"
+    },
+    {
+        contract_id: "26DD0145",
+        program2: "Irish",
+        plan: "Renzon"
+    },
+    {
+        contract_id: "26DD0162",
+        program2: "Ashlie",
+        plan: "Kemmuel"
+    },
+    {
+        contract_id: "26DD0164",
+        program2: "James",
+        plan: "Renzon"
+    },
+    {
+        contract_id: "26DD0174",
+        program2: "Rizza",
+        plan: "Reynalyn"
+    },
+    {
+        contract_id: "26DD0175",
+        program2: "Christian",
+        plan: "Kressel"
+    },
+    {
+        contract_id: "26DD0211",
+        program2: "Bevelyn",
+        plan: "Renzon"
+    },
+    {
+        contract_id: "26DD0212",
+        program2: "Ashlie",
+        plan: "Kemmuel"
+    }
+];
